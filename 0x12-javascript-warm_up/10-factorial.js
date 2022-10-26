27abaf6825c1ee4cd1a6215512082ca35602f425
Divine-bright <brightheybukah@gmail.com>
2022-10-26 09:50:07 +0100

    Create 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const a = process.argv[2];
+function factorial (a) {
+  if (isNaN(a) || a === 1) {
+    return (1);
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
+console.log(factorial(parseInt(a)));
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Sec
